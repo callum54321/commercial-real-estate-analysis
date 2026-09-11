e25e3737df031dbb855a1264015fe0290e288ceb
Callum Barkus <callumb54321@gmail.com>
2026-09-11 22:43:40 +0800

    new: enhanced initial scraped data cleaning procedure

---
+++ scraped_data_cleaning.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import re
+
+# CSV to dataframe
+df = pd.read_csv("listings.csv")
+
+# Drop missing values
+df.dropna(inplace=True)
+
+# Sq ft value extraction
+df["sq_ft"] = df["all_info"].str.extract(r"\bSize:\s+([\d,]+)\s+s.f\b")
+df["sq_ft"] = df["sq_ft"].str.replace(",", "")
+df["sq_ft"] = df["sq_ft"].astype(int)
+
+# Total price value extraction
+df["price"] = df["price"].str.replace(",", "")
+df["price_total"] = df["price"].astype(int)
+
+# Price per sq ft
+df["price_sq_ft"] = df["price_total"] / df["sq_ft"]
+df["price_sq_ft"] = df["price_sq_ft"].astype(int)
+
+# Drop all_info column
+df = df.drop(columns=["all_info"])
